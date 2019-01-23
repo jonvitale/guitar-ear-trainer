0156d00688f349e9d25bb0b78d78b4c8f489c482
Jonathan Michael Vitale (District Performance Office) <jvitale@ADMIN.PHILASD.NET>
2019-01-23 14:30:35 -0500

    initial commit
    angular project with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import { 
+	MatButtonModule, 
+	MatIconModule,
+} from '@angular/material';
+
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+  ],
+  exports: [
+  	MatButtonModule,
+  	MatIconModule,
+  ]
+})
+export class MaterialModule { }
